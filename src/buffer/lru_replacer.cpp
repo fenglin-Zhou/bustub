f33841653d818cd3ac539da423d228c0e0f1fb1e
fenglin-Zhou <fenglin0114@qq.com>
2022-03-13 22:05:55 +0800

    p1,task1

--- src/buffer/lru_replacer.cpp
+++ src/buffer/lru_replacer.cpp
@@ -14,16 +14,47 @@
 
 namespace bustub {
 
-LRUReplacer::LRUReplacer(size_t num_pages) {}
+LRUReplacer::LRUReplacer(size_t num_pages) { capacity_ = num_pages; }
 
 LRUReplacer::~LRUReplacer() = default;
 
-bool LRUReplacer::Victim(frame_id_t *frame_id) { return false; }
+bool LRUReplacer::Victim(frame_id_t *frame_id) {
+  std::unique_lock<std::mutex> lock(mtx_);
+  if (lru_map_.empty()) {
+    return false;
+  }
+  // select the last object and delete
+  frame_id_t last_frame = lru_list_.back();
+  lru_map_.erase(last_frame);
+  lru_list_.pop_back();
+  // no bug, But I think last_frame may be destroyed.
+  *frame_id = last_frame;
+  return true;
+}
 
-void LRUReplacer::Pin(frame_id_t frame_id) {}
+void LRUReplacer::Pin(frame_id_t frame_id) {
+  std::unique_lock<std::mutex> lock(mtx_);
+  if (lru_map_.find(frame_id) != lru_map_.end()) {
+    lru_list_.erase(lru_map_[frame_id]);
+    lru_map_.erase(frame_id);
+  }
+}
 
-void LRUReplacer::Unpin(frame_id_t frame_id) {}
+void LRUReplacer::Unpin(frame_id_t frame_id) {
+  std::unique_lock<std::mutex> lock(mtx_);
+  if (lru_map_.find(frame_id) != lru_map_.end()) {
+    return;
+  }
+  while (Size() >= capacity_) {
+    frame_id_t to_del = lru_list_.back();
+    lru_list_.pop_back();
+    lru_map_.erase(to_del);
+  }
+  // insert
+  lru_list_.push_front(frame_id);
+  lru_map_[frame_id] = lru_list_.begin();
+}
 
-size_t LRUReplacer::Size() { return 0; }
+size_t LRUReplacer::Size() { return lru_list_.size(); }
 
 }  // namespace bustub
--- src/include/buffer/lru_replacer.h
+++ src/include/buffer/lru_replacer.h
@@ -13,6 +13,7 @@
 #pragma once
 
 #include <list>
+#include <map>
 #include <mutex>  // NOLINT
 #include <vector>
 
@@ -47,6 +48,10 @@ class LRUReplacer : public Replacer {
 
  private:
   // TODO(student): implement me!
+  std::mutex mtx_;
+  size_t capacity_;
+  std::list<frame_id_t> lru_list_;
+  std::map<frame_id_t, std::list<frame_id_t>::iterator> lru_map_;
 };
 
 }  // namespace bustub
